751acc7827c82e471886042f1e000d82f5c4286a
Jeffrey Yurkiw <jyurkiw@yahoo.com>
2017-12-02 13:54:00 -0800

    Initial Import

---
+++ Assets/scripts/UI/LoadScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+	public void ButtonHandler(string name)
+    {
+        Debug.Log(name);
+        SceneManager.LoadScene(name, LoadSceneMode.Single);
+    }
+}
